6094f497f568e0805409d41e59228cf22e51c910
P Skolsky <patrickskolskyart@gmail.com>
2020-02-07 19:18:20 -0500

    Updates

---
+++ precache-manifest.068ffc3211c198cad959ecb0da7becdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39613f541963a475678ee0f34fea67b2",
+    "url": "/Click-Game-React-App/index.html"
+  },
+  {
+    "revision": "ce51f1047ef39eeed11b",
+    "url": "/Click-Game-React-App/static/css/main.f6eed54b.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/Click-Game-React-App/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Click-Game-React-App/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce51f1047ef39eeed11b",
+    "url": "/Click-Game-React-App/static/js/main.2b861bed.chunk.js"
+  },
+  {
+    "revision": "0bc663efb26e05a61c4e",
+    "url": "/Click-Game-React-App/static/js/runtime-main.1e3796e0.js"
+  },
+  {
+    "revision": "eee679af6a9519e09f00cb2ae795030e",
+    "url": "/Click-Game-React-App/static/media/athf_bg.eee679af.jpeg"
+  }
+]);
